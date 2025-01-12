ee182ed3f639598ec8fe83b068db13bec1d87c8f
Puhachov Kyrylo <147053916+KyryloPugach@users.noreply.github.com>
2025-01-13 00:14:15 +0200

    Add files via upload

---
+++ Task2/arkpz-pzpi-22-10-puhachov-kyrylo-task2/server/Bridges/Bridges/Models/Bridge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bridges.Models
+{
+    public class Bridge
+    {
+        public int BridgeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BridgeType { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<Sensor> Sensors { get; set; }
+        public List<Inspector> Inspectors { get; set; }
+    }
+}
---
+++ Task2/arkpz-pzpi-22-10-puhachov-kyrylo-task2/server/Bridges/Bridges/Models/Sensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bridges.Models
+{
+    public class Sensor
+    {
+        public int SensorId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime InstallationDate { get; set; }
+        public int BridgeId { get; set; }
+        public Bridge Bridge { get; set; }
+        public int SensorTypeId { get; set; }
+        public SensorType SensorType { get; set; }
+        public List<SensorData> SensorDatas { get; set; }
+    }
+}
